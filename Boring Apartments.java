07a680c75f1195e454a6fec38d3ea6983c16bc8c
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2020-10-20 21:56:12 +0530

    Create Boring Apartments.java

---
+++ Boring Apartments.java
@@ -0,0 +1,131 @@
+package CodeforcesBeastModeOn;
+
+import java.util.Scanner;
+
+public class BoringApartments {
+
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while(t-->0) {
+			int x = scn.nextInt();
+			if(x==1) {
+				System.out.println(1);
+			}
+			else	if(x==11) {
+				System.out.println(3);
+			}
+			else	if(x==111) {
+				System.out.println(6);
+			}
+			else	if(x==1111) {
+				System.out.println(10);
+			}
+
+			else	if(x==2) {
+				System.out.println(11);
+			}
+			else	if(x==22) {
+				System.out.println(13);
+			}
+			else	if(x==222) {
+				System.out.println(16);
+			}
+			else	if(x==2222) {
+				System.out.println(20);
+			}
+			else	if(x==3) {
+				System.out.println(21);
+			}
+			else	if(x==33) {
+				System.out.println(23);
+			}
+			else	if(x==333) {
+				System.out.println(26);
+			}
+			else	if(x==3333) {
+				System.out.println(30);
+			}
+			else	if(x==4) {
+				System.out.println(31);
+			}
+			else	if(x==44) {
+				System.out.println(33);
+			}
+			else	if(x==444) {
+				System.out.println(36);
+			}
+			else	if(x==4444) {
+				System.out.println(40);
+			}
+			else	if(x==5) {
+				System.out.println(41);
+			}
+			else	if(x==55) {
+				System.out.println(43);
+			}
+			else	if(x==555) {
+				System.out.println(46);
+			}
+			else	if(x==5555) {
+				System.out.println(50);
+			}
+			else	if(x==6) {
+				System.out.println(51);
+			}
+			else	if(x==66) {
+				System.out.println(53);
+			}
+			else	if(x==666) {
+				System.out.println(56);
+			}
+			else	if(x==6666) {
+				System.out.println(60);
+			}
+			else	if(x==7) {
+				System.out.println(61);
+			}
+			else	if(x==77) {
+				System.out.println(63);
+			}
+			else	if(x==777) {
+				System.out.println(66);
+			}
+			else	if(x==7777) {
+				System.out.println(70);
+			}
+			else	if(x==8) {
+				System.out.println(71);
+			}
+			else	if(x==88) {
+				System.out.println(73);
+			}
+			else	if(x==888) {
+				System.out.println(76);
+			}
+			else	if(x==8888) {
+				System.out.println(80);
+			}
+			else	if(x==9) {
+				System.out.println(81);
+			}
+			else	if(x==99) {
+				System.out.println(83);
+			}
+			else	if(x==999) {
+				System.out.println(86);
+			}
+			else	if(x==9999) {
+				System.out.println(90);
+			}
+
+
+
+
+
+
+		}
+
+	}
+
+}
